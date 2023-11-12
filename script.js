885b7934808f4e405f84987cb6e1a95cfb84e990
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-12 21:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	// I have sorted the array
+	arr.sort((a, b)=>a-b);
+
+	//creating a variable for storing total cost
+	const total=0;
+
+	//need loop for iterate
+
+	while (arr.length>1) {
+		let min1=arr.shift();
+		let min2=arr.shift();
+
+		let costOfTwo= min1+min2;
+
+		// i nead to add this total
+		total=total+costOfTwo;
+
+		//need to push this total into the arr
+		arr.push(costOfTwo);
+
+		arr.sort((a, b)=>a-b);
+		
+		
+	}
+	return total;
   
 }
 
